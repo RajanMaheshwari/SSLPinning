d892e16cfb1547f1c8f1df4cbfd1bc08f57d0dbf
Rajan Maheshwari <rajanmaheshwari1990@gmail.com>
2022-11-26 19:09:39 +0530

    url append query items old ios versions handling

--- SSLPinningDemo/ViewController.swift
+++ SSLPinningDemo/ViewController.swift
@@ -32,13 +32,21 @@ class ViewController: UIViewController {
         
         var url = URL.init(string: "https://api.openweathermap.org/data/2.5/weather")
         
-        url?.append(queryItems: [
+        let queryItems = [
             URLQueryItem.init(name: "lat", value: "28.7041"),
             URLQueryItem.init(name: "lon", value: "77.1025"),
             URLQueryItem.init(name: "units", value: "metric"),
             URLQueryItem.init(name: "appid", value: "26f1ffa29736dc1105d00b93743954d2"),
-            
-        ])
+        ]
+        
+        if #available(iOS 16.0, *) {
+            url?.append(queryItems: queryItems)
+        } else {
+            // Fallback on earlier versions
+            var components = URLComponents.init(url: url!, resolvingAgainstBaseURL: false)
+            components?.queryItems = queryItems
+            url = components?.url
+        }
         
         NetworkManager.shared.afRequest(url: url, expecting: WeatherResponse.self) { [weak self] data, error in
             
